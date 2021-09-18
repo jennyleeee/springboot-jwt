dc06fcdd7a6b1959cb621827b1825292db68d43a
jennyleeee <jjaeun2@naver.com>
2021-09-18 19:48:55 +0900

    first commit

---
+++ src/test/kotlin/me/jennyleeee/tutorial/JwtTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jennyleeee.tutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
